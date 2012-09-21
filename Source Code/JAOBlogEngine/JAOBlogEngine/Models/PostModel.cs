2f8f99528da9252970fb4c99d1897651c0a9a442
sergey-olshansky <sotodyrail@gmail.com>
2012-09-20 23:56:37 -0700

    Интегрировать задачи [14],[17] в Source Code

---
+++ Source Code/JAOBlogEngine/JAOBlogEngine/Models/PostModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JAOBlogEngine.Models
+{
+    [ValidateInput(false)]
+    public class PostModel : Controller
+    {
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public string Announce { get; set; }
+        public string Detail { get; set; }
+        public ActionResult EditPost()
+        {
+            return View();
+        }
+    }
+
+    [ValidateInput(false)]
+    public class GetModel : Controller
+    {
+        public string Time { get; set; }
+        public int Id { get; set; }
+        public string _User { get; set; }
+        public string Title { get; set; }
+        public string Announce { get; set; }
+        public string Detail { get; set; }
+        public ActionResult EditPost()
+        {
+            return View();
+        }
+    }
+ }
